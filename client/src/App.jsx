d86df00c9a5d6b50c7745fc35276de2d191a8adc
Bhumika864 <rajputbhumika121@gmail.com>
2025-08-06 18:13:18 +0530

    Login component created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,13 +7,14 @@ import AddBlog from './pages/admin/AddBlog';
 import ListBlog from './pages/admin/ListBlog';
 import Comments from './pages/admin/Comments';
 import Layout from './pages/admin/Layout';
+import Login from './components/admin/Login';
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/blog/:id' element={<Blog/>}/>
-        <Route path='/admin' element={<Layout/>}>
+        <Route path='/admin' element={false? <Layout/>:<Login/>}>
         <Route index element={<Dashboard/>}/>
         <Route path="addBlog" element={<AddBlog/>}/>
         <Route path='listBlog' element={ListBlog}/>
